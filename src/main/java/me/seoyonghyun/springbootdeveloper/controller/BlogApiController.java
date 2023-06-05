cefc99cce2df7f7f8c4f27b43b369399adb9515b
SYH <syh05247@naver.com>
2023-06-05 16:57:34 +0900

    feat: updateArticle 메서드 추가
    controller 계층에서 id와 UpdateArticleRequest 를 인자로 클라이언트에게 받고 그에 따른 게시글을 수정하는 updateArticle 메서드 추가

--- src/main/java/me/seoyonghyun/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/seoyonghyun/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.seoyonghyun.springbootdeveloper.domain.Article;
 import me.seoyonghyun.springbootdeveloper.dto.AddArticleRequest;
 import me.seoyonghyun.springbootdeveloper.dto.ArticleResponse;
+import me.seoyonghyun.springbootdeveloper.dto.UpdateArticleRequest;
 import me.seoyonghyun.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -51,5 +52,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(
+            @PathVariable long id, @RequestBody UpdateArticleRequest request) {
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
