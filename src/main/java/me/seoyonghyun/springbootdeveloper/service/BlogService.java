a96a94f32f76cb6710d07e965f4e3fbb6516f867
SYH <syh05247@naver.com>
2023-06-05 15:55:09 +0900

    feat: BlogService findAll 메서드 추가
    모든 게시글에 목록을 가져오기 위한 findAll 메서드 추가

--- src/main/java/me/seoyonghyun/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/seoyonghyun/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.seoyonghyun.springbootdeveloper.dto.AddArticleRequest;
 import me.seoyonghyun.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -14,6 +16,10 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 
 }
