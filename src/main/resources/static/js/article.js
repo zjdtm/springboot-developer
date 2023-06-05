c77f05ced12deaa728f753b0799b76ebbb696ab0
SYH <syh05247@naver.com>
2023-06-06 01:57:18 +0900

    feat: article.js 등록 기능 추가
    등록 기능 추가에 따른 articleList.html 에 등록 버튼을 추가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -38,3 +38,23 @@ if(modifyButton) {
             })
     })
 }
+
+// 등록 기능
+const createButton = document.getElementById("create-btn");
+if(createButton) {
+    createButton.addEventListener("click", () => {
+        fetch("/api/articles", {
+            method: 'POST',
+            headers : {
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value,
+            }),
+        }).then(() => {
+            alert("등록 완료되었습니다.");
+            location.replace("/articles");
+        })
+    })
+}
