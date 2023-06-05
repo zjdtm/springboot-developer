4032eafe31b8c5a752c9b3c9f6d614e473c0420a
SYH <syh05247@naver.com>
2023-06-06 01:51:01 +0900

    feat: article.js 수정 기능 추가
    javascript 추가에 따른 article.html 에 modify-btn 도 추가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -13,3 +13,28 @@ if(deleteButton) {
             });
     });
 }
+
+// 수정 기능
+const modifyButton = document.getElementById('modify-btn');
+
+if(modifyButton) {
+    modifyButton.addEventListener('click', () => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert('수정이 완료되었습니다.');
+                location.replace(`/articles/${id}`);
+            })
+    })
+}
