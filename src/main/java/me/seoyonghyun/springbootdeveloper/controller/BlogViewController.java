0efbf956f5a26e50d9b43125f54c0e2e3a894485
SYH <syh05247@naver.com>
2023-06-06 01:24:08 +0900

    feat: BlogViewController 추가
    getArticles, getArticle 메서드는 각 각 게시글 목록, 게시글 조회 를 해주는 메서드로 Model 에 데이터를 넣어준다.

---
+++ src/main/java/me/seoyonghyun/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,39 @@
+package me.seoyonghyun.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.seoyonghyun.springbootdeveloper.domain.Article;
+import me.seoyonghyun.springbootdeveloper.dto.ArticleListViewResponse;
+import me.seoyonghyun.springbootdeveloper.dto.ArticleViewResponse;
+import me.seoyonghyun.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+}
