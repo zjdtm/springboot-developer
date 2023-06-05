0cd13e647ae635d10032df9674988b872be74401
SYH <syh05247@naver.com>
2023-06-05 16:43:51 +0900

    feat: update 메서드 추가
    수정할 게시글의 id 와 UpdateArticleRequest 매개변수로 받아서 수정하는 update 메서드 추가

--- src/main/java/me/seoyonghyun/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/seoyonghyun/springbootdeveloper/service/BlogService.java
@@ -1,8 +1,10 @@
 package me.seoyonghyun.springbootdeveloper.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.seoyonghyun.springbootdeveloper.domain.Article;
 import me.seoyonghyun.springbootdeveloper.dto.AddArticleRequest;
+import me.seoyonghyun.springbootdeveloper.dto.UpdateArticleRequest;
 import me.seoyonghyun.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +31,16 @@ public class BlogService {
 
     public void delete(long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 
 }
