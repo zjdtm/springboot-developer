bde7e5adb1882d00087458c2a9080c4cc2846448
SYH <syh05247@naver.com>
2023-06-06 19:48:32 +0900

    feat: UserService 객체 생성
    UserRepository, BCryptPasswordEncoder 를 필드로 가지고 있으며 회원가입을 위한 save() 메서드 로직 추가

---
+++ src/main/java/me/seoyonghyun/springbootdeveloper/service/UserService.java
@@ -0,0 +1,25 @@
+package me.seoyonghyun.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.seoyonghyun.springbootdeveloper.domain.User;
+import me.seoyonghyun.springbootdeveloper.dto.AddUserRequest;
+import me.seoyonghyun.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+
+    }
+
+}
