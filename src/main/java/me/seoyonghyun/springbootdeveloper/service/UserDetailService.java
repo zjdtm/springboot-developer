0f63e8ecbb4049bfa9cd4cccb5fe3fea7f9e853a
SYH <syh05247@naver.com>
2023-06-06 19:28:36 +0900

    feat: UserDetailService 객체 생성
    UserDetailsService 를 구현하는 UserDetailService 는 오버라이딩 하는 loadUserByUsername 메서드는 email 을 매개변수로 받아서 user 를 리턴해준다.

---
+++ src/main/java/me/seoyonghyun/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package me.seoyonghyun.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.seoyonghyun.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
