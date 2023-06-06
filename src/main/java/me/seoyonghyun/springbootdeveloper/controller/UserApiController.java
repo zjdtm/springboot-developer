d4bb24a37b8c2292115fe8e6216e60da8930da31
SYH <syh05247@naver.com>
2023-06-06 20:07:16 +0900

    feat: logout 메서드 추가
    SecurityContextLogoutHandler 의 logout() 메서드를 호출해 로그아웃하는 로직 추가

--- src/main/java/me/seoyonghyun/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/seoyonghyun/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.seoyonghyun.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.seoyonghyun.springbootdeveloper.dto.AddUserRequest;
 import me.seoyonghyun.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +20,12 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 
