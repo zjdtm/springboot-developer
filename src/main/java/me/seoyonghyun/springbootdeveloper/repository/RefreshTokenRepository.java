7fb111814d5f031d01cded3e5eff5aa0d99fdce3
SYH <syh05247@naver.com>
2023-06-09 16:26:09 +0900

    feat: RefreshTokenRepository 추가
    findByUserId, findByRefreshToken 메서드 추가

---
+++ src/main/java/me/seoyonghyun/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package me.seoyonghyun.springbootdeveloper.repository;
+
+import me.seoyonghyun.springbootdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
