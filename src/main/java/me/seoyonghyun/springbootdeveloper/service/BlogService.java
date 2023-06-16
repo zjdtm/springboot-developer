671568bfd9746571f5a9bcd541d9f3e06098e1df
SYH <syh05247@naver.com>
2023-06-17 01:21:17 +0900

    feat: delete, update 시 유저 검증
    authorizeArticleAuthor 메서드를 이용해서 SecurityContextHolder 에서 사용자의 정보를 가져와 인증한다.

--- src/main/java/me/seoyonghyun/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/seoyonghyun/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,7 @@ import me.seoyonghyun.springbootdeveloper.domain.Article;
 import me.seoyonghyun.springbootdeveloper.dto.AddArticleRequest;
 import me.seoyonghyun.springbootdeveloper.dto.UpdateArticleRequest;
 import me.seoyonghyun.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +31,10 @@ public class BlogService {
     }
 
     public void delete(long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not fount : " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
@@ -38,9 +42,19 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if(!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 
 }
