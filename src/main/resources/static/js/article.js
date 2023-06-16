637a5efe3f4fb2382054006b4088101af9729790
SYH <syh05247@naver.com>
2023-06-17 01:11:21 +0900

    refactor: 삭제 기능, 수정 기능 수정
    httpRequest 함수를 이용해서 등록, 수정, 삭제 기능에 반복적인 코드 없애기

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,60 +1,118 @@
 // 삭제 기능
 const deleteButton = document.getElementById('delete-btn');
 
-if(deleteButton) {
+if (deleteButton) {
     deleteButton.addEventListener('click', () => {
         let id = document.getElementById('article-id').value;
-        fetch(`/api/articles/${id}`, {
-            method: 'DELETE'
-        })
-            .then(() => {
-                alert('삭제가 완료되었습니다.');
-                location.replace('/articles');
-            });
+        function success() {
+            alert("삭제가 완료되었습니다.");
+            location.replace("/articles");
+        }
+
+        function fail() {
+            alert("삭제 실패했습니다.");
+            location.replace("/articles");
+        }
+
+        httpRequest("DELETE", "/api/articles/" + id, null, success, fail);
     });
 }
 
 // 수정 기능
 const modifyButton = document.getElementById('modify-btn');
 
-if(modifyButton) {
+if (modifyButton) {
     modifyButton.addEventListener('click', () => {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
 
-        fetch(`/api/articles/${id}`, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value
-            })
-        })
-            .then(() => {
-                alert('수정이 완료되었습니다.');
-                location.replace(`/articles/${id}`);
-            })
-    })
+        body = JSON.stringify({
+            title: document.getElementById("title").value,
+            content: document.getElementById("content").value,
+        });
+
+        function success() {
+            alert("수정 완료되었습니다.");
+            location.replace("/articles/" + id);
+        }
+
+        httpRequest("PUT", "/api/articles/" + id, body, success, fail);
+    });
 }
 
 // 등록 기능
 const createButton = document.getElementById("create-btn");
-if(createButton) {
+if (createButton) {
     createButton.addEventListener("click", () => {
-        fetch("/api/articles", {
-            method: 'POST',
-            headers : {
-                "Content-Type" : "application/json",
-            },
-            body : JSON.stringify({
-                title: document.getElementById("title").value,
-                content: document.getElementById("content").value,
-            }),
-        }).then(() => {
+        body = JSON.stringify({
+            title: document.getElementById("title").value,
+            content: document.getElementById("content").value,
+        });
+
+        function success() {
             alert("등록 완료되었습니다.");
             location.replace("/articles");
-        })
+        }
+
+        function fail() {
+            alert("등록 실패했습니다.");
+            location.replace("/articles");
+        }
+
+
+    })
+}
+
+function getCookie(key) {
+    var result = null;
+    var cookie = document.cookie.split(";");
+    cookie.some(function (item) {
+        item = item.replace(" ", "");
+
+        var dic = item.split("=");
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+    return result;
+}
+
+// HTTP 요청을 보내는 함수
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, {
+        method: method,
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access_token"),
+            "Content-Type": "application/json",
+        },
+        body: body,
+    }).then((response) => {
+        if (response.status === 200 || response.status === 201) {
+            return success();
+        }
+        const refresh_token = getCookie("refresh_token");
+        if (response.status === 401 && refresh_token) {
+            fetch("/api/token", {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("access_token"),
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    refresh_token: getCookie("refresh_token"),
+                }),
+            }).then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+            }).then((result) => {
+                localStorage.setItem("access_token", result.accessToken);
+                httpRequest(method, url, body, success, fail);
+            }).catch((error) => fail());
+        } else {
+            return fail();
+        }
     })
 }
